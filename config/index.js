a7057e3bf32ab6f0c5c91430b207fd2bdd0cdd74
zhangyudan <zhangyudan@chainwaytsp.com>
2018-10-11 20:46:26 +0800

    解决冲突

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.1.107', // can be overwritten by process.env.HOST
+
+    host: '192.168.0.133', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
